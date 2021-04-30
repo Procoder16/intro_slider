1ef4b4d333c6457f4da16b54983cbda3971b8fb7
Procoder16 <smsoumik1608@gmail.com>
2021-04-30 11:57:34 +0530

    imported the slider widget

--- flutter_intro_slider/lib/homePage.dart
+++ flutter_intro_slider/lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intro_slider/intro_slider.dart';
 
 class HomePage extends StatefulWidget {
   @override
--- flutter_intro_slider/lib/main.dart
+++ flutter_intro_slider/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intro_slider/intro_slider.dart';
 import 'homePage.dart';
 
 void main() {
